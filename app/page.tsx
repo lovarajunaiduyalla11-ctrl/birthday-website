00c797ad4ee48d4e972e17f049abffa5225c3ca5
lovarajunaiduyalla11-ctrl <lovarajunaiduyalla11@gmail.com>
2026-06-13 14:50:10 +0530

    added code -2

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,53 @@
 import Hero from "@/components/Hero";
-import Timeline from "@/components/Timeline";
+import RelationshipCounter from "@/components/RelationshipCounter";
+import StorySection from "@/components/StorySection";
 import Gallery from "@/components/Gallery";
-import FutureDreams from "@/components/FutureDreams";
 import LoveLetter from "@/components/LoveLetter";
+
+import { story } from "@/lib/content";
 
 export default function Home() {
   return (
     <main>
+
       <Hero />
-      <Timeline />
+
+      <RelationshipCounter />
+
+      <StorySection
+        title={story.beginning.title}
+        text={story.beginning.text}
+      />
+
+      <StorySection
+        title={story.admiration.title}
+        text={story.admiration.text}
+      />
+
       <Gallery />
-      <FutureDreams />
+
+      <StorySection
+        title={story.challenge.title}
+        text={story.challenge.text}
+      />
+
+      <StorySection
+        title={story.waiting.title}
+        text={story.waiting.text}
+      />
+
+      <StorySection
+        title={story.future.title}
+        text={story.future.text}
+      />
+
+      <StorySection
+        title={story.gratitude.title}
+        text={story.gratitude.text}
+      />
+
       <LoveLetter />
+
     </main>
   );
 }
