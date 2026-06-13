0ae1a4c7fb572183f9d05b2f23ecc219c9ef04c5
lovarajunaiduyalla11-ctrl <lovarajunaiduyalla11@gmail.com>
2026-06-13 13:43:46 +0530

    added code -1

---
+++ lib/images.ts
@@ -0,0 +1,19 @@
+export const images = {
+  hero: "https://via.placeholder.com/600x800",
+
+  memories: [
+    "https://via.placeholder.com/600x800",
+    "https://via.placeholder.com/600x800",
+    "https://via.placeholder.com/600x800",
+    "https://via.placeholder.com/600x800",
+    "https://via.placeholder.com/600x800",
+  ],
+
+  gallery: [
+    "https://via.placeholder.com/600x800",
+    "https://via.placeholder.com/600x800",
+    "https://via.placeholder.com/600x800",
+    "https://via.placeholder.com/600x800",
+    "https://via.placeholder.com/600x800",
+  ],
+};
