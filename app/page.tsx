49d0dd5bdb281d2c8aee4903ab7810495c107e9c
lovarajunaiduyalla11-ctrl <lovarajunaiduyalla11@gmail.com>
2026-06-18 16:21:27 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,47 +7,85 @@ import LoveLetter from "@/components/LoveLetter";
 import { story } from "@/lib/content";
 
 export default function Home() {
-  return (
-    <main>
+return ( <main>
 
-      <Hero />
+```
+  <Hero />
 
-      <RelationshipCounter />
+  <RelationshipCounter />
 
-      <StorySection
-        title={story.beginning.title}
-        text={story.beginning.text}
-      />
+  <StorySection
+    title={story.beginning.title}
+    text={story.beginning.text}
+  />
 
-      <StorySection
-        title={story.admiration.title}
-        text={story.admiration.text}
-      />
+  <StorySection
+    title={story.admiration.title}
+    text={story.admiration.text}
+  />
 
-      <Gallery />
+  <StorySection
+    title={story.firstMeeting.title}
+    text={story.firstMeeting.text}
+  />
 
-      <StorySection
-        title={story.challenge.title}
-        text={story.challenge.text}
-      />
+  <StorySection
+    title={story.firstStorm.title}
+    text={story.firstStorm.text}
+  />
 
-      <StorySection
-        title={story.waiting.title}
-        text={story.waiting.text}
-      />
+  <StorySection
+    title={story.journey.title}
+    text={story.journey.text}
+  />
 
-      <StorySection
-        title={story.future.title}
-        text={story.future.text}
-      />
+  <Gallery />
 
-      <StorySection
-        title={story.gratitude.title}
-        text={story.gratitude.text}
-      />
+  <StorySection
+    title={story.proposal.title}
+    text={story.proposal.text}
+  />
 
-      <LoveLetter />
+  <StorySection
+    title={story.challenge.title}
+    text={story.challenge.text}
+  />
 
-    </main>
-  );
+  <StorySection
+    title={story.promise.title}
+    text={story.promise.text}
+  />
+
+  <StorySection
+    title={story.jobAchievement.title}
+    text={story.jobAchievement.text}
+  />
+
+  <StorySection
+    title={story.waiting.title}
+    text={story.waiting.text}
+  />
+
+  <StorySection
+    title={story.whyStayed.title}
+    text={story.whyStayed.text}
+  />
+
+  <StorySection
+    title={story.future.title}
+    text={story.future.text}
+  />
+
+  <StorySection
+    title={story.gratitude.title}
+    text={story.gratitude.text}
+  />
+
+  <LoveLetter />
+
+</main>
+```
+
+);
 }
+
