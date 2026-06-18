2619ad3b8966b71fe9b783c6f70d5e87dc2dfa0f
lovarajunaiduyalla11-ctrl <lovarajunaiduyalla11@gmail.com>
2026-06-18 18:04:40 +0530

    Update images.ts

--- lib/images.ts
+++ lib/images.ts
@@ -1,24 +1,27 @@
 export const images = {
-hero: "/images/14.jpg",
+  hero: "/images/birthday-image.jpg",
 
-couple: "/images/couple-us.jpg",
+  couple: "/images/couple-us.jpg",
 
-gallery: [
-"/images/01.jpg",
-"/images/02.jpg",
-"/images/03.jpg",
-"/images/04.jpg",
-"/images/05.jpg",
-"/images/06.jpg",
-"/images/07.jpg",
-"/images/08.jpeg",
-"/images/09.jpg",
-"/images/10.jpg",
-"/images/11.jpg",
-"/images/12.jpg",
-"/images/13.jpg",
-"/images/friends-1.jpg",
-"/images/friends-2.jpg",
-"/images/birthday-image.jpg"
-]
+  gallery: [
+    "/images/birthday-image.jpg",
+    "/images/couple-us.jpg",
+
+    "/images/01.jpg",
+    "/images/02.jpg",
+    "/images/03.jpg",
+    "/images/04.jpg",
+    "/images/05.jpg",
+    "/images/06.jpg",
+    "/images/07.jpg",
+    "/images/08.jpeg",
+    "/images/09.jpg",
+    "/images/10.jpg",
+    "/images/11.jpg",
+    "/images/12.jpg",
+    "/images/13.jpg",
+
+    "/images/friends-1.jpg",
+    "/images/friends-2.jpg"
+  ]
 };
