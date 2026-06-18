130d1930e870dbe312f0ba553e664d54104c16e1
lovarajunaiduyalla11-ctrl <lovarajunaiduyalla11@gmail.com>
2026-06-18 17:49:40 +0530

    Update images.ts

--- lib/images.ts
+++ lib/images.ts
@@ -1,19 +1,24 @@
 export const images = {
-  hero: "https://via.placeholder.com/600x800",
+hero: "/images/14.jpg",
 
-  memories: [
-    "https://via.placeholder.com/600x800",
-    "https://via.placeholder.com/600x800",
-    "https://via.placeholder.com/600x800",
-    "https://via.placeholder.com/600x800",
-    "https://via.placeholder.com/600x800",
-  ],
+couple: "/images/couple-us.jpg",
 
-  gallery: [
-    "https://via.placeholder.com/600x800",
-    "https://via.placeholder.com/600x800",
-    "https://via.placeholder.com/600x800",
-    "https://via.placeholder.com/600x800",
-    "https://via.placeholder.com/600x800",
-  ],
+gallery: [
+"/images/01.jpg",
+"/images/02.jpg",
+"/images/03.jpg",
+"/images/04.jpg",
+"/images/05.jpg",
+"/images/06.jpg",
+"/images/07.jpg",
+"/images/08.jpeg",
+"/images/09.jpg",
+"/images/10.jpg",
+"/images/11.jpg",
+"/images/12.jpg",
+"/images/13.jpg",
+"/images/friends-1.jpg",
+"/images/friends-2.jpg",
+"/images/birthday-image.jpg"
+]
 };
